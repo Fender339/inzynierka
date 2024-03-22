3bff2dac2fdb7ccc2f1ecc6c7ce621c9a0d57923
Fender339 <olafnowak33@gmail.com>
2024-03-22 20:54:55 +0100

    add delete, change request forwarding, add multiple insert and update

--- forwarder.js
+++ forwarder.js
@@ -4,6 +4,15 @@ const cors = require('cors');
 const mysql = require('mysql');
 const moment = require('moment')
 
+function sendQuery( sqlQuery ) {
+    connection.query(sqlQuery, [true], (error, results, fields) => {
+        if (error) return console.error(error.message);
+    });
+}
+
+function generateRandomComment() {
+    return ( ( Math.random() + 1 ).toString(36).substring(7) );
+}
 
 const app = express();
 const port = 3000;
@@ -29,16 +38,36 @@ let connection = mysql.createConnection({
 
 app.post('/', async (req, res) => {
     try {
-        let start = moment().milliseconds()
-        const sqlQuery = req.body;
+        const queryType = req.body;
 
-        if ( !sqlQuery ) {
-            return res.status(400).send({ error: `Missing required parameters ${sqlQuery}` });
+        if ( !queryType ) {
+            return res.status(400).send({ error: `Missing required parameters ${queryType}` });
         }
-        connection.query(sqlQuery, [true], (error, results, fields) => {
-            if (error) return console.error(error.message);
-            res.send(`Time of execution: ${moment().milliseconds() - start}`);
-        });
+        let sqlQuery;
+        switch ( queryType )
+        {
+            case 'select': 
+                sqlQuery = 'select * from testtable';
+                sendQuery(sqlQuery);
+                break;
+            case 'insert': 
+                for(let i = 0; i < 100000; i++) {  
+                    sqlQuery = `insert into testtable (date, comment, number) values ('2024-01-01', '${ generateRandomComment() }', ${ ( Math.random() + i) })`;
+                    sendQuery(sqlQuery);
+                }
+                break;
+            case 'update': 
+                for(let i = 0; i < 100000; i++) {
+                    sqlQuery = `update testtable set comment='${ generateRandomComment() }' where id = ${ i+1 }`;
+                    sendQuery(sqlQuery); 
+                }
+                break;
+            case 'delete': 
+                sqlQuery = `delete from testtable`;
+                sendQuery(sqlQuery);
+                break;
+        }
+        res.send(``);
     } catch (error) {
         console.error('Error forwarding SQL query:', error.message);
         res.status(500).send({ error: 'Internal server error' });
